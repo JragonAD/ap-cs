ce32ec72c48bdaff43f4f37a50d497cd237f4697
jazhang <jazhang@ncolonie.org>
2021-10-01 09:36:05 -0400

    did quiz for control structures

---
+++ Control Structures/Program3.java
@@ -0,0 +1,16 @@
+public class Program1 {
+    public static void main(String[] args) {
+        int count1 = 0, count2 = 0, iterator = 1;
+        for (int i = 0; i < 11; i++) {
+            count1 += iterator;
+            iterator *= -1;
+            count2 += iterator; 
+        }
+        System.out.println(count1 - count2);
+
+
+        for (int n = 50; n > 0; n = n / 2) {
+            System.out.println(n);
+        }
+    }
+}
