7ef4e50299572bfd78e0c8b418382929056c3a46
jazhang <jazhang@ncolonie.org>
2021-12-08 09:28:35 -0500

    updated iso trap for inheritance lab

--- Lab - Inheritance/IsoscelesTrapezoid.java
+++ Lab - Inheritance/IsoscelesTrapezoid.java
@@ -5,7 +5,7 @@ public class IsoscelesTrapezoid extends Trapezoid {
     }
 
     public boolean isIsoTrape() {
-        return (slopes[1] == slopes[3] && points[0].getDist(points[1]) == points[2].getDist(points[3]))
-                || (slopes[0] == slopes[2] && points[1].getDist(points[2]) == points[3].getDist(points[4]));
+        return (slopes[1] == slopes[3] && dists[0] == dists[2])
+                || (slopes[0] == slopes[2] && dists[1] == dists[3]);
     }
 }
