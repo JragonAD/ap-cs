5fd204eae341142121128225c25a662f124b7923
jazhang <jazhang@ncolonie.org>
2022-01-19 09:48:07 -0500

    activity 2

--- AP Lab - ELEVENS 2022/Activity 2 Starter Code/Deck.java
+++ AP Lab - ELEVENS 2022/Activity 2 Starter Code/Deck.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 /**
  * The Deck class represents a shuffled deck of cards.
  * It provides several operations including
- *      initialize, shuffle, deal, and check if empty.
+ * initialize, shuffle, deal, and check if empty.
  */
 public class Deck {
 
@@ -20,34 +20,43 @@ public class Deck {
 	 */
 	private int size;
 
-
 	/**
 	 * Creates a new <code>Deck</code> instance.<BR>
 	 * It pairs each element of ranks with each element of suits,
 	 * and produces one of the corresponding card.
-	 * @param ranks is an array containing all of the card ranks.
-	 * @param suits is an array containing all of the card suits.
+	 * 
+	 * @param ranks  is an array containing all of the card ranks.
+	 * @param suits  is an array containing all of the card suits.
 	 * @param values is an array containing all of the card point values.
 	 */
 	public Deck(String[] ranks, String[] suits, int[] values) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		cards = new ArrayList<Card>();
+		for (String suit : suits)
+			for (int i = 0; i < ranks.length; i++)
+				cards.add(new Card(ranks[i], suit, values[i]));
+		size = cards.size();
+		shuffle();
 	}
-
 
 	/**
 	 * Determines if this deck is empty (no undealt cards).
+	 * 
 	 * @return true if this deck is empty, false otherwise.
 	 */
 	public boolean isEmpty() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		return size == 0;
 	}
 
 	/**
 	 * Accesses the number of undealt cards in this deck.
+	 * 
 	 * @return the number of undealt cards in this deck.
 	 */
 	public int size() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		return size;
 	}
 
 	/**
@@ -60,15 +69,19 @@ public class Deck {
 
 	/**
 	 * Deals a card from this deck.
+	 * 
 	 * @return the card just dealt, or null if all the cards have been
 	 *         previously dealt.
 	 */
 	public Card deal() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		size -= 1;
+		return size < 0 ? null : cards.get(size);
 	}
 
 	/**
 	 * Generates and returns a string representation of this deck.
+	 * 
 	 * @return a string representation of this deck.
 	 */
 	@Override
--- AP Lab - ELEVENS 2022/Activity 2 Starter Code/DeckTester.java
+++ AP Lab - ELEVENS 2022/Activity 2 Starter Code/DeckTester.java
@@ -5,9 +5,29 @@ public class DeckTester {
 
 	/**
 	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
+	 * 
+	 * @param args is not used.
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		String[] ranks = { "jack", "queen", "king" };
+		String[] suits = { "blue", "red" };
+		int[] pointValues = { 11, 12, 13 };
+		Deck d1 = new Deck(ranks, suits, pointValues);
+		System.out.println(d1.size());
+
+		String[] ranks2 = {};
+		String[] suits2 = {};
+		int[] pointValues2 = {};
+		Deck d2 = new Deck(ranks2, suits2, pointValues2);
+		System.out.println(d2.isEmpty() + "\n");
+
+		String[] ranks3 = { "jack", "queen", "king" };
+		String[] suits3 = { "blue", "red" };
+		int[] pointValues3 = { 11, 12, 13 };
+		Deck d3 = new Deck(ranks3, suits3, pointValues3);
+		System.out.println(d3);
+		System.out.println("Dealing: " + d3.deal());
+		System.out.println(d3);
 	}
 }
