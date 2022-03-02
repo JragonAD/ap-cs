702bdac1d9d9326e892ce98d3007f61df34c86e1
jazhang <jazhang@ncolonie.org>
2022-03-02 10:00:02 -0500

    added picture filters

--- AP Lab - PICTURE 2022/pixLab/classes/PictureTester.java
+++ AP Lab - PICTURE 2022/pixLab/classes/PictureTester.java
@@ -2,85 +2,113 @@ package classes;
 
 /**
  * This class contains class (static) methods
- * that will help you test the Picture class 
- * methods.  Uncomment the methods and the code
+ * that will help you test the Picture class
+ * methods. Uncomment the methods and the code
  * in the main to test.
  * 
- * @author Barbara Ericson 
+ * @author Barbara Ericson
  */
-public class PictureTester
-{
+public class PictureTester {
   /** Method to test zeroBlue */
-  public static void testZeroBlue()
-  {
+  public static void testZeroBlue() {
     Picture beach = new Picture("beach.jpg");
-    beach.explore();
+    // beach.explore();
     beach.zeroBlue();
     beach.explore();
   }
-  
+
+  // USER CODE START
+
+  /** Method to test keepOnlyBlue */
+  public static void testKeepOnlyBlue() {
+    Picture beach = new Picture("beach.jpg");
+    // beach.explore();
+    beach.keepOnlyBlue();
+    beach.explore();
+  }
+
+  public static void testNegate() {
+    Picture beach = new Picture("beach.jpg");
+    // beach.explore();
+    beach.negate();
+    beach.explore();
+  }
+
+  public static void testGrayscale() {
+    Picture beach = new Picture("beach.jpg");
+    // beach.explore();
+    beach.grayscale();
+    beach.explore();
+  }
+
+  public static void testFixUnderwater() {
+    Picture water = new Picture("water.jpg");
+    water.explore();
+    water.fixUnderwater(40, 160);
+    water.explore();
+  }
+
+  // USER CODE END
+
   /** Method to test mirrorVertical */
-  public static void testMirrorVertical()
-  {
+  public static void testMirrorVertical() {
     Picture caterpillar = new Picture("caterpillar.jpg");
     caterpillar.explore();
     caterpillar.mirrorVertical();
     caterpillar.explore();
   }
-  
+
   /** Method to test mirrorTemple */
-  public static void testMirrorTemple()
-  {
+  public static void testMirrorTemple() {
     Picture temple = new Picture("temple.jpg");
     temple.explore();
     temple.mirrorTemple();
     temple.explore();
   }
-  
+
   /** Method to test the collage method */
-  public static void testCollage()
-  {
+  public static void testCollage() {
     Picture canvas = new Picture("640x480.jpg");
     canvas.createCollage();
     canvas.explore();
   }
-  
+
   /** Method to test edgeDetection */
-  public static void testEdgeDetection()
-  {
+  public static void testEdgeDetection() {
     Picture swan = new Picture("swan.jpg");
     swan.edgeDetection(10);
     swan.explore();
   }
-  
-  /** Main method for testing.  Every class can have a main
-    * method in Java */
-  public static void main(String[] args)
-  {
+
+  /**
+   * Main method for testing. Every class can have a main
+   * method in Java
+   */
+  public static void main(String[] args) {
     // uncomment a call here to run a test
     // and comment out the ones you don't want
     // to run
     testZeroBlue();
-    //testKeepOnlyBlue();
-    //testKeepOnlyRed();
-    //testKeepOnlyGreen();
-    //testNegate();
-    //testGrayscale();
-    //testFixUnderwater();
-    //testMirrorVertical();
-    testMirrorTemple();
-    //testMirrorArms();
-    //testMirrorGull();
-    //testMirrorDiagonal();
-    //testCollage();
-    //testCopy();
-    //testEdgeDetection();
-    //testEdgeDetection2();
-    //testChromakey();
-    //testEncodeAndDecode();
-    //testGetCountRedOverValue(250);
-    //testSetRedToHalfValueInTopHalf();
-    //testClearBlueOverValue(200);
-    //testGetAverageForColumn(0);
+    testKeepOnlyBlue();
+    // testKeepOnlyRed();
+    // testKeepOnlyGreen();
+    testNegate();
+    testGrayscale();
+    testFixUnderwater();
+    // testMirrorVertical();
+    // testMirrorTemple();
+    // testMirrorArms();
+    // testMirrorGull();
+    // testMirrorDiagonal();
+    // testCollage();
+    // testCopy();
+    // testEdgeDetection();
+    // testEdgeDetection2();
+    // testChromakey();
+    // testEncodeAndDecode();
+    // testGetCountRedOverValue(250);
+    // testSetRedToHalfValueInTopHalf();
+    // testClearBlueOverValue(200);
+    // testGetAverageForColumn(0);
   }
 }
